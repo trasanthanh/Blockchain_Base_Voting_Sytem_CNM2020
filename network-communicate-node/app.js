5d1f770d5fe9adacd643002a7c242022d81d835c
trasanthanh <44344701+trasanthanh@users.noreply.github.com>
2020-06-21 10:29:56 +0700

    remove routes

--- network-communicate-node/app.js
+++ network-communicate-node/app.js
@@ -4,9 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
@@ -46,7 +43,7 @@ io.on("connection", function (socket) {
 			}
 		}
 		if(resElement){
-			resElement.client.emit('get-result', resElement.ballot)
+			resElement.client.emit('get-result', resElement.ballot);
 		}
 	})
 });
@@ -59,10 +56,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
